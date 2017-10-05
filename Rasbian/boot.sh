9131d544de1d261f07e31970ae6bb816818fb0fc
Arky <hitmanarky@gmail.com>
2017-10-05 16:26:11 +0700

    Adding network interfaces to boot.sh

--- Rasbian/boot.sh
+++ Rasbian/boot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 qemu-system-arm -kernel kernel-qemu-4.4.34-jessie -cpu arm1176 -m 256 -machine versatilepb \
  -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw vga=normal console=ttyAMA0" \
- -drive "file=./2017-09-07-raspbian-stretch-lite.img,index=0,media=disk,format=raw" -no-reboot -serial stdio -curses
+ -drive "file=./2017-09-07-raspbian-stretch-lite.img,index=0,media=disk,format=raw" -no-reboot -serial stdio -curses \
+ -net nic,macaddr=52:54:00:12:34:57 -net nic,macaddr=52:54:00:12:34:58  -net socket,connect=127.0.0.1:1234
